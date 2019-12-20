3c56c2a09d679a3e56253418919e89564e022f30
TravelFiend <mikegrace274@gmail.com>
2019-12-19 21:11:57 -0800

    refactored to use middleware

--- __tests__/trip.test.js
+++ __tests__/trip.test.js
@@ -4,7 +4,7 @@ const superagent = require('superagent');
 const connect = require('../lib/utils/connect');
 const request = require('supertest');
 const mongoose = require('mongoose');
-const getWeather = require('../lib/services/weather');
+const { getWeather, getWoeid } = require('../lib/services/weather');
 const Trip = require('../lib/models/Trip');
 const Itineraries = require('../lib/models/Itineraries');
 
@@ -14,11 +14,11 @@ describe('Trip routes', () => {
     });
 
     beforeEach(() => {
-        mongoose.connection.dropDatabase();
+        return mongoose.connection.dropDatabase();
     });
 
     afterAll(() => {
-        mongoose.connection.close();
+        return mongoose.connection.close();
     });
 
     it('should create a new trip', () => {
@@ -102,15 +102,16 @@ describe('Trip routes', () => {
             returnToCity: 'Portland'
         });
         
-        const itinerary = await Itineraries.create([
+        const itinerary = await Itineraries.create(
             {
                 tripId: trip._id,
                 event: 'Mountain biking',
                 eventDate: new Date('January 10, 2020'),
                 eventCity: 'Hamburg',
-                eventDaysDuration: 3
+                eventDaysDuration: 3,
+                woeid: 2475687
             }
-        ]);
+        );
         return request(app)
             .get(`/api/v1/trip/${trip._id}`)
             .then(res => {
@@ -122,7 +123,7 @@ describe('Trip routes', () => {
                     arriveCity: 'Hamburg',
                     returnFromCity: 'Prague',
                     returnToCity: 'Portland',
-                    itineraries: JSON.parse(JSON.stringify(itinerary)),
+                    itineraries: [JSON.parse(JSON.stringify(itinerary))],
                     __v: 0
                 });
             });
@@ -185,24 +186,116 @@ describe('Trip routes', () => {
     });
 
     it('can get a woeid for a city', () => {
-        return superagent
-            .get('metaweather.com/api/location/search/?query=Portland')
+        return getWoeid('Portland')
             .then(res => {
-                expect(res.body[0].woeid).toEqual(2475687);
+                expect(res).toEqual(2475687);
             });
     });
 
     it('can get weather using woeid and date', () => {
-        const date = new Date('October 4, 2011');
+        const date = new Date('February 21, 2019');
         const woeid = 2475687;
         return superagent
             .get(`metaweather.com/api/location/${woeid}/${date.getFullYear()}/${date.getMonth() + 1}/${date.getDate()}`)
             .then(res => {
-                expect(res.body[0].woeid).toEqual(2475687);
+                expect(res.body[0]).toEqual({
+                    id: 4779895051780096,
+                    weather_state_name: 'Heavy Cloud',
+                    weather_state_abbr: 'hc',
+                    wind_direction_compass: 'NNE',
+                    created: '2019-02-22T08:30:34.546252Z',
+                    applicable_date: '2019-02-21',
+                    min_temp: -1.9900000000000002,
+                    max_temp: 8.6,
+                    the_temp: 7.195,
+                    wind_speed: 4.351856345101937,
+                    wind_direction: 30.99303878738074,
+                    air_pressure: 1018.31,
+                    humidity: 71,
+                    visibility: 6.904055316949018,
+                    predictability: 71
+                });
             });
     });
 
     it('returns weather body', () => {
-        expect(getWeather('Cleveland', 2010, 10, 4)).toEqual('this');
+        getWeather(2475687, 2017, 10, 4)
+            .then(res => {
+                expect(res).toEqual({
+                    id: 4779895051780096,
+                    weather_state_name: 'Heavy Cloud',
+                    weather_state_abbr: 'hc',
+                    wind_direction_compass: 'NNE',
+                    created: '2019-02-22T08:30:34.546252Z',
+                    applicable_date: '2019-02-21',
+                    min_temp: -1.9900000000000002,
+                    max_temp: 8.6,
+                    the_temp: 7.195,
+                    wind_speed: 4.351856345101937,
+                    wind_direction: 30.99303878738074,
+                    air_pressure: 1018.31,
+                    humidity: 71,
+                    visibility: 6.904055316949018,
+                    predictability: 71
+                });
+            });
+    });
+
+    it('creates an itinerary item', async() => {
+        const trip = await Trip.create({
+            departDate: new Date('January 1, 2020'),
+            returnDate: new Date('February 4, 2020'),
+            departCity: 'Portland',
+            arriveCity: 'Hamburg',
+            returnFromCity: 'Prague',
+            returnToCity: 'Portland'
+        });
+        
+        return request(app)
+            .post(`/api/v1/trip/${trip._id}/day-plan`)
+            .send({
+                tripId: trip._id,
+                event: 'Snowboard the Alps',
+                eventDate: new Date('January 2, 2020'),
+                eventCity: 'Prague',
+                eventDaysDuration: 2,
+                woeid: 2475687
+            })
+            .then(res => {
+                expect(res.body.itineraries).toEqual([{
+                    '__v': 0,
+                    '_id': expect.any(String),
+                    'event': 'Snowboard the Alps',
+                    'eventCity': 'Prague',
+                    'eventDate': '2020-01-02T08:00:00.000Z',
+                    'eventDaysDuration': 2,
+                    'tripId': expect.any(String),
+                    'woeid': 796597
+                }]);
+            });
+    });
+
+    it('should delete an itinerary item by id', async() => {
+        const trip = await Trip.create({
+            departDate: new Date('January 1, 2020'),
+            returnDate: new Date('February 4, 2020'),
+            departCity: 'Portland',
+            arriveCity: 'Hamburg',
+            returnFromCity: 'Prague',
+            returnToCity: 'Portland'
+        });
+        const itinerary = await Itineraries.create({
+            tripId: trip._id,
+            event: 'Snowboard the Alps',
+            eventDate: new Date('January 2, 2020'),
+            eventCity: 'Stockholm',
+            eventDaysDuration: 2,
+            woeid: 2475687
+        });
+        return request(app)
+            .delete(`/api/v1/trip/${trip._id}/day-plan/${itinerary._id}`)
+            .then(res => {
+                expect(res.body.itineraries).toHaveLength(0);
+            });
     });
 });
